5d3529b8a234c21c703666aa9fb6ebcd7c58d615
GuarU345 <abel_chicharito@hotmail.com>
2023-04-29 14:43:21 -0600

    [UPD] change the style of pokemon-details component

--- src/components/PokemonDetails.jsx
+++ src/components/PokemonDetails.jsx
@@ -1,42 +1,50 @@
-import React, { useState,useEffect} from 'react'
-import {getPokemonDetails} from "../functions/api"
+import React, { useState, useEffect } from "react";
+import { getPokemonDetails } from "../functions/api";
 
 const PokemonDetails = () => {
+  const [pokemondetails, setPokemonDetails] = useState([]);
 
-  const [pokemondetails,setPokemonDetails] = useState([])
+  const selectedPokemon = JSON.parse(localStorage.getItem("selectedPokemon"));
+  const pokemon_name = selectedPokemon.name;
+  const url_pokemon = selectedPokemon.url;
 
-  const selectedPokemon = JSON.parse(localStorage.getItem("selectedPokemon"))
-  const pokemon_name = selectedPokemon.name
-  const url_pokemon = selectedPokemon.url
-
-  useEffect(()=>{
-    const fetchPokemon = async() => {
-        const data = await getPokemonDetails(pokemon_name,url_pokemon)
-        setPokemonDetails(data)
-    }
-    fetchPokemon()
-  },[])
+  useEffect(() => {
+    const fetchPokemon = async () => {
+      const data = await getPokemonDetails(pokemon_name, url_pokemon);
+      setPokemonDetails(data);
+    };
+    fetchPokemon();
+  }, []);
 
   return (
     <>
-      <div className="pokemon-details">
-          <div className={`pokemon-card-details ${pokemondetails.type}`}>
-          <div className="pokemon-header-details">
-            <h5>{pokemondetails.name}</h5>
-            <img className='img-fluid w-50' src={pokemondetails.image}></img>
-            <div className='pkd-information'>
-              <h5 style={{color:'white'}}>Information of Pokemon</h5>
-              <span>Type: {pokemondetails.type}</span>
-              <h5 style={{color:'red'}}>Principal Atacks</h5>
-              <span>attack 1: {pokemondetails.move1}</span>
-              <span>attack 2: {pokemondetails.move2}</span>
-              <span>attack 3: {pokemondetails.move3}</span>
-            </div>
+      <section className="pokemon-details">
+        <article className={`pokemon-card-details ${pokemondetails.type}`}>
+          <header className="pokemon-header-details">
+            <h2>{pokemondetails.name}</h2>
+            <img
+              className="img-fluid w-50"
+              src={pokemondetails.image}
+              alt={`Picture of ${pokemondetails.name}`}
+            />
+          </header>
+          <div className="pkd-information">
+            <h3>Information of Pokemon</h3>
+            <dl>
+              <dt>Type:</dt>
+              <dd className="pkd-type">{pokemondetails.type}</dd>
+            </dl>
+            <h3>Principal Attacks</h3>
+            <ul className="pkd-attack-list">
+              <li className="pkd-attack">{pokemondetails.move1}</li>
+              <li className="pkd-attack">{pokemondetails.move2}</li>
+              <li className="pkd-attack">{pokemondetails.move3}</li>
+            </ul>
           </div>
-        </div>
-      </div>
+        </article>
+      </section>
     </>
-  )
-}
+  );
+};
 
-export default PokemonDetails
+export default PokemonDetails;
